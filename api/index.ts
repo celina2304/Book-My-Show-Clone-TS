fdab67814a1d233d83bc9085a26157b075d27d87
celina2304 <celina23042001@gmail.com>
2024-11-29 16:24:55 +0530

    deploying to vercel - 3

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
 import path from 'path';
 
 const app = express();
+
+// Create __filename and __dirname for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Serve static files
 const clientPath = path.join(__dirname, '../client/dist');
